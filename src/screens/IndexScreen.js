9e9bbd420555dede670821b9ac27e5136297a17f
ELREYS <gdilisa@hotmail.com>
2019-11-15 14:50:33 +0100

    Start Template with Navigation and Gesture Handler

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+
+
+const IndexScreen = () =>{
+
+
+return (
+
+<View>
+    <Text>Index Screen</Text>
+</View>
+
+);
+}
+
+
+const styles = StyleSheet.create({
+
+
+})
+
+
+export default IndexScreen;
