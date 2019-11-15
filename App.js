25d6ee448b00da530300b991636c544efc163610
ELREYS <gdilisa@hotmail.com>
2019-11-15 14:55:36 +0100

    Change App with a export components

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
@@ -11,4 +12,8 @@ const navigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App/>
+}
