d5373c09746feffe02d3f753dc1e59258c44c4e9
ELREYS <gdilisa@hotmail.com>
2019-11-18 13:46:25 +0100

    add a little bit style

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext, createRef } from "react";
 import { FlatList, Text, StyleSheet, View, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -8,31 +9,39 @@ const IndexScreen = () => {
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <FlatList
-        style={styles.container}
         data={state}
         keyExtractor={state => state.id}
         renderItem={({ item }) => (
-          <Text>
-            {item.blog} {item.id}
-          </Text>
+          <View style={styles.container}>
+            <Text style={styles.title}>
+              {item.blog} {item.id}
+            </Text>
+            <Feather style={styles.icon} name="trash" />
+          </View>
         )}
       />
-      <Button
-        title="Add post"
-        onPress={addBlogPost}
-      />
+      <Button title="Add post" onPress={addBlogPost} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#f2f2f2",
-    marginLeft: 10,
-    marginTop: 15,
-    marginRight: 15
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    paddingVertical:20,
+    borderTopWidth:1,
+    borderBottomWidth:1,
+    borderColor:'black',
+    backgroundColor: "#f2f2f2", 
+  },title:{
+      fontSize:18,
+     marginLeft:10
+
+  },
+  icon:{
+      fontSize:24
   }
 });
 
